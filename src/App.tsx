45beb524a8237c8d7819bd35521ed8230bb179d2
Gabriel <gabrieltkdnobrega63@gmail.com>
2025-08-02 18:41:49 -0300

    feat: integrate ReactFlow for dynamic node and edge management in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,58 @@
+import {
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+  ReactFlow
+} from '@xyflow/react'
+import '@xyflow/react/dist/style.css'
+import { useCallback, useState } from 'react'
+import { AppContextMenu } from './components/app-context-menu'
 import { ControlBar } from './components/control-bar'
 
+const initialNodes = [
+  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+  { id: 'n3', position: { x: 0, y: 200 }, data: { label: 'Node 3' } }
+]
+const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }]
+
 export default function App() {
+  const [nodes, setNodes] = useState(initialNodes)
+  const [edges, setEdges] = useState(initialEdges)
+
+  const onNodesChange = useCallback(
+    (changes) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    []
+  )
+  const onEdgesChange = useCallback(
+    (changes) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    []
+  )
+  const onConnect = useCallback(
+    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    []
+  )
   return (
-    <div
-      className="w-screen h-screen bg-background text-muted-foreground"
-      style={{
-        backgroundImage: 'radial-gradient(currentColor 1px, transparent 1px)',
-        backgroundSize: '15px 15px'
-      }}
-    >
-      <ControlBar />
-    </div>
+    <AppContextMenu>
+      <div
+        className="w-screen h-screen bg-background text-muted-foreground"
+        style={{
+          backgroundImage: 'radial-gradient(currentColor 1px, transparent 1px)',
+          backgroundSize: '15px 15px'
+        }}
+      >
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          fitView
+        />
+        <ControlBar />
+      </div>
+    </AppContextMenu>
   )
 }
