5c58b6327c4ce7d70b11db49c075fcb942ceb215
Gabriel <gabrieltkdnobrega63@gmail.com>
2025-08-02 20:01:12 -0300

    refactor: simplify App component structure by integrating NodesProvider and EdgesProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,22 @@
-import {
-  addEdge,
-  applyEdgeChanges,
-  applyNodeChanges,
-  ReactFlow,
-  type Edge,
-  type Node,
-  type OnConnect,
-  type OnEdgesChange,
-  type OnNodesChange
-} from '@xyflow/react'
-import '@xyflow/react/dist/style.css'
-import { useCallback, useState } from 'react'
 import { AppContextMenu } from './components/app-context-menu'
 import { ControlBar } from './components/control-bar'
+import { Flow } from './components/flow'
 
-const initialNodes = [
-  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
-  { id: 'n3', position: { x: 0, y: 200 }, data: { label: 'Node 3' } }
-]
-const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }]
+import '@xyflow/react/dist/style.css'
+import { EdgesProvider } from './context/edges-context'
+import { NodesProvider } from './context/nodes-context'
 
 export default function App() {
-  const [nodes, setNodes] = useState<Node[]>(initialNodes)
-  const [edges, setEdges] = useState<Edge[]>(initialEdges)
-
-  const onNodesChange: OnNodesChange = useCallback(
-    (changes) =>
-      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
-    []
-  )
-  const onEdgesChange: OnEdgesChange = useCallback(
-    (changes) =>
-      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
-    []
-  )
-  const onConnect: OnConnect = useCallback(
-    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
-    []
-  )
   return (
-    <AppContextMenu>
-      <div
-        className="w-screen h-screen bg-background text-muted-foreground"
-        style={{
-          backgroundImage: 'radial-gradient(currentColor 1px, transparent 1px)',
-          backgroundSize: '15px 15px'
-        }}
-      >
-        <ReactFlow
-          nodes={nodes}
-          edges={edges}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-          onConnect={onConnect}
-          fitView
-        />
-        <ControlBar />
-      </div>
-    </AppContextMenu>
+    <NodesProvider>
+      <EdgesProvider>
+        <AppContextMenu>
+          <main className="w-screen h-screen bg-background text-muted-foreground">
+            <Flow />
+            <ControlBar />
+          </main>
+        </AppContextMenu>
+      </EdgesProvider>
+    </NodesProvider>
   )
 }
