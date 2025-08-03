7b6c5f6b761ff5c200b3d40f7085da644e4c5d3a
Gabriel <gabrieltkdnobrega63@gmail.com>
2025-08-02 21:06:30 -0300

    feat: integrate Header component into App layout and restructure context providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import { AppContextMenu } from './components/app-context-menu'
-import { ControlBar } from './components/control-bar'
 import { Flow } from './components/flow'
 
 import '@xyflow/react/dist/style.css'
+import { Header } from './components/header'
 import { EdgesProvider } from './context/edges-context'
 import { NodesProvider } from './context/nodes-context'
+import { TitleProvider } from './context/title-context'
 
 export default function App() {
   return (
     <NodesProvider>
       <EdgesProvider>
-        <AppContextMenu>
-          <main className="w-screen h-screen bg-background text-muted-foreground">
-            <Flow />
-            <ControlBar />
-          </main>
-        </AppContextMenu>
+        <TitleProvider>
+          <AppContextMenu>
+            <main className="w-screen h-screen bg-background text-muted-foreground">
+              <Header />
+              <Flow />
+            </main>
+          </AppContextMenu>
+        </TitleProvider>
       </EdgesProvider>
     </NodesProvider>
   )
