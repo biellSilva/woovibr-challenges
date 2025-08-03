4fdd989a97598328d586bda1802c8453462a9964
Gabriel <gabrieltkdnobrega63@gmail.com>
2025-08-02 22:14:55 -0300

    feat: wrap application in ReactFlowProvider for improved context management

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import { AppContextMenu } from './components/app-context-menu'
 import { Flow } from './components/flow'
 
+import { ReactFlowProvider } from '@xyflow/react'
 import '@xyflow/react/dist/style.css'
 import { Header } from './components/header'
 import { EdgesProvider } from './context/edges-context'
+import { MousePosProvider } from './context/mouse-pos-context'
 import { NodesProvider } from './context/nodes-context'
 import { TitleProvider } from './context/title-context'
 
 export default function App() {
   return (
-    <NodesProvider>
-      <EdgesProvider>
-        <TitleProvider>
-          <AppContextMenu>
-            <main className="w-screen h-screen bg-background text-muted-foreground">
-              <Header />
-              <Flow />
-            </main>
-          </AppContextMenu>
-        </TitleProvider>
-      </EdgesProvider>
-    </NodesProvider>
+    <ReactFlowProvider>
+      <NodesProvider>
+        <EdgesProvider>
+          <TitleProvider>
+            <MousePosProvider>
+              <AppContextMenu>
+                <main className="w-screen h-screen bg-background text-muted-foreground">
+                  <Header />
+                  <Flow />
+                </main>
+              </AppContextMenu>
+            </MousePosProvider>
+          </TitleProvider>
+        </EdgesProvider>
+      </NodesProvider>
+    </ReactFlowProvider>
   )
 }
