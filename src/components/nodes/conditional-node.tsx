7403dc729794e02484f2efc0abab40cd236e5474
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-08-05 16:59:33 -0300

    feat: refactor ConditionalNode component for improved structure and styling

--- src/components/nodes/conditional-node.tsx
+++ src/components/nodes/conditional-node.tsx
@@ -1,20 +1,30 @@
+import { handleClassName } from '@/constants/handle'
 import { Handle, Position } from '@xyflow/react'
+import { Textarea } from '../ui/textarea'
 
 export const ConditionalNode = () => {
   return (
-    <div className="bg-gray-100 min-w-fit w-18 text-center p-2 rounded-sm shadow-md">
-      <Handle className="" type="target" position={Position.Left} />
-      <input
-        className="text-muted-foreground outline-0"
-        placeholder="Condition"
+    <div className="bg-white min-w-18 w-fit text-start p-2 rounded-sm shadow-md">
+      <div className="flex flex-col w-fit">
+        <span className="text-muted-foreground outline-0">Condition</span>
+        <Textarea className="!bg-gray-200 focus-visible:ring-0 border-0 rounded-xs p-1 !text-xs text-muted min-w-18 w-fit min-h-8 !h-fit" />
+      </div>
+
+      <Handle
+        id="input"
+        className={handleClassName('target')}
+        type="target"
+        position={Position.Left}
       />
       <Handle
-        className="!bg-background !border-green-500"
+        id="true"
+        className="!bg-green-500"
         type="source"
         position={Position.Right}
       />
       <Handle
-        className="!bg-background !border-red-500 "
+        id="false"
+        className="!bg-red-500"
         type="source"
         position={Position.Bottom}
       />
