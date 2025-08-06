a0cd27aa60c73b6b1a85654ec3ed6476b5b82e12
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-08-06 16:20:43 -0300

    feat: add addNode function to NodesContext for dynamic node creation

--- src/context/nodes-context.tsx
+++ src/context/nodes-context.tsx
@@ -1,9 +1,17 @@
-import type { Node } from '@xyflow/react'
-import { createContext, useState } from 'react'
+import { useReactFlow, type Node } from '@xyflow/react'
+import { createContext, useContext, useState } from 'react'
+import MousePosContext from './mouse-pos-context'
+
+export interface AddNodeProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: Record<string, any>
+  type: string
+}
 
 interface ContextProps {
   nodes: Node[]
   setNodes: React.Dispatch<React.SetStateAction<Node[]>>
+  addNode: (props: AddNodeProps) => void
 }
 
 const NodesContext = createContext<ContextProps>({} as ContextProps)
@@ -21,9 +29,21 @@ export const NodesProvider: React.FC<{ children: React.ReactNode }> = ({
   children
 }) => {
   const [nodes, setNodes] = useState<Node[]>(initialNodes)
+  const { position } = useContext(MousePosContext)
+  const { screenToFlowPosition } = useReactFlow()
+
+  const addNode = ({ data, type }: AddNodeProps) => {
+    const node: Node = {
+      id: `n${nodes.length + 1}`,
+      position: screenToFlowPosition(position),
+      data,
+      type
+    }
+    setNodes((prevNodes) => [...prevNodes, node])
+  }
 
   return (
-    <NodesContext.Provider value={{ nodes, setNodes }}>
+    <NodesContext.Provider value={{ nodes, setNodes, addNode }}>
       {children}
     </NodesContext.Provider>
   )
