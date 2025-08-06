4dec472a39418a95a0d15592fa32d85967f4daff
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-08-06 16:17:15 -0300

    feat: enhance ConditionalNode component with props and dynamic label rendering

--- src/components/nodes/conditional-node.tsx
+++ src/components/nodes/conditional-node.tsx
@@ -1,13 +1,32 @@
 import { handleClassName } from '@/constants/handle'
-import { Handle, Position } from '@xyflow/react'
+import { Handle, Position, useReactFlow } from '@xyflow/react'
 import { Textarea } from '../ui/textarea'
 
-export const ConditionalNode = () => {
+interface ConditionalNodeProps {
+  id: string
+}
+
+export const ConditionalNode = ({ id }: ConditionalNodeProps) => {
+  const { updateNodeData, getNode } = useReactFlow()
+
+  const node = getNode(id)
+
+  if (!node) {
+    return null
+  }
+
   return (
-    <div className="bg-white min-w-18 w-fit text-start p-2 rounded-sm shadow-md">
+    <div className="bg-white min-w-18 w-fit h-fit text-start p-2 rounded-sm shadow-md">
       <div className="flex flex-col w-fit">
-        <span className="text-muted-foreground outline-0">Condition</span>
-        <Textarea className="!bg-gray-200 focus-visible:ring-0 border-0 rounded-xs p-1 !text-xs text-muted min-w-18 w-fit min-h-8 !h-fit" />
+        <span className="text-muted-foreground text-xs font-semibold mb-1">
+          {typeof node.data.label === 'string' ? node.data.label : ''}
+        </span>
+        <Textarea
+          onChange={(e) => {
+            updateNodeData(id, { content: e.target.value })
+          }}
+          className="!bg-gray-200 focus-visible:ring-0 border-0 rounded-xs p-1 !text-xs text-muted min-w-full max-w-fit min-h-8 !h-fit"
+        />
       </div>
 
       <Handle
