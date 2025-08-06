bf8ef0fee79a8640df6e9bd9a2a527f3c5765fe1
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-08-06 16:17:08 -0300

    feat: add Controls component to Flow for enhanced user interaction

--- src/components/flow.tsx
+++ src/components/flow.tsx
@@ -7,6 +7,7 @@ import {
   applyEdgeChanges,
   applyNodeChanges,
   Background,
+  Controls,
   ReactFlow,
   type OnConnect,
   type OnEdgesChange,
@@ -47,6 +48,7 @@ export const Flow = () => {
       }}
     >
       <Background />
+      <Controls />
     </ReactFlow>
   )
 }
