75178657b315fcbf5c53860eede115291cc6ec09
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-08-06 16:16:50 -0300

    feat: update StartNode component styling for improved appearance

--- src/components/nodes/start-node.tsx
+++ src/components/nodes/start-node.tsx
@@ -2,8 +2,8 @@ import { Handle, Position } from '@xyflow/react'
 
 export const StartNode = () => {
   return (
-    <div className="bg-gray-100 min-w-fit w-18 text-center p-2 rounded-sm shadow-md">
-      <span className="text-muted-foreground">Start</span>
+    <div className="bg-gray-100 min-w-fit w-18 text-center p-2 rounded-full justify-center items-center flex shadow-md">
+      <span className="text-muted-foreground text-xs font-semibold">Start</span>
       <Handle
         className="!bg-background !border-purple-500"
         type="source"
